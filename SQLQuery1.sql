02a06798a11a2458e28597e578cc7b014f37543a
Eduardo <131829588+Eduardo-Capatti@users.noreply.github.com>
2025-10-31 14:46:39 -0300

    adicionando mais condicoes de status em compras

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -87,7 +87,7 @@ create table Compras(
 	idCompra     int			not null     primary key identity,
 	dataCompra	 date			not null	 default GETDATE(),	  
 	totalCompra	 decimal(10,2)      null,
-	statusCompra int				null	 check(statusCompra in(1,2,3)),
+	statusCompra int				null	 check(statusCompra in(0,1,2,3,4)) -- 0 = carrinho, 1 = aguardando pagamento, 2 = pago, 3 = enviado, 4 = entregue,
 	data_entrega date			not null,
 	idCliente int not null references Clientes(idPessoa),
 	idFuncionario int not null references Funcionarios(idPessoa)
@@ -113,4 +113,5 @@ create table Itens_Entradas(
 	valorIE money null,
 	qtdIE	int null,
 	primary key(idProduto, idEntrada)
+
 )
