f3fd7f1780f4335b2e06996067b20e64683c495f
GuilhermeFranci <gui.frchagas@gmail.com>
2025-11-14 20:55:57 -0300

    Update ClienteController.cs

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,30 +1,98 @@
-// using Microsoft.AspNetCore.Mvc;
-// using RNStore.Repositories;
-// using RNStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using RNStore.Models;
+using RNStore.Repositories;
+using Microsoft.Data.SqlClient; 
 
-// namespace RNStore.Controllers;
+namespace RNStore.Controllers;
 
-// public class CatalogoController : Controller
-// {
+public class ClienteController : Controller
+{
+    private IClienteRepository repository;
 
-//     private ICatalogoRepository repository;
+    public ClienteController(IClienteRepository repository)
+    {
+        this.repository = repository;
+    }
 
-//     public CatalogoController (ICatalogoRepository repository)
-//     {
-//         this.repository = repository;
-//     }
-//     public ActionResult Index()
-//     {
-//         var sliders = repository.ReadSlides();
+    [HttpGet]
+    public ActionResult Cadastro()
+    {
+        return View(); 
+    }
 
-//         ViewBag.slides = sliders;
-        
-//         return View(repository.Read());
-//     }
+    [HttpPost]
+    public ActionResult Create(Cliente cliente)
+    {
+        try
+        {
+            string confirmarSenha = Request.Form["ConfirmarSenha"];
+            if (cliente.Senha != confirmarSenha)
+            {
+                ViewBag.Error = "As senhas não conferem. Tente novamente.";
+                return View("Cadastro", cliente); 
+            }
+            
+            bool TemCampoNulo(Cliente cli)
+            {
+                if (cli == null) return true;
 
+                return cli.GetType()
+                    .GetProperties()
+                    .Where(p => p.CanRead &&
+                                p.Name != "IdPessoa" &&
+                                p.Name != "Enderecos" &&
+                                p.Name != "Telefone") 
+                    .Any(p => p.GetValue(cli) == null);
+            }
 
-//     public ActionResult Produto(int idProduto, int idCalcado)
-//     {
-//         return View(repository.Read(idProduto, idCalcado));
-//     }
-// }
+            if (TemCampoNulo(cliente))
+            {
+                ViewBag.Error = "Preencha todas as informações obrigatórias!";
+                return View("Cadastro", cliente); 
+            }
+
+            repository.Create(cliente); 
+
+            return RedirectToAction("Index", "Catalogo"); 
+        }
+        catch (SqlException e)
+        {
+            if (e.Message.Contains("UNIQUE KEY"))
+            {
+                ViewBag.Error = "Este E-mail ou CPF já está em uso.";
+            }
+            else
+            {
+                ViewBag.Error = "Ocorreu um erro no banco de dados ao salvar.";
+            }
+            return View("Cadastro", cliente);
+        }
+        catch(Exception e)
+        {
+            ViewBag.Error = "Ocorreu um erro inesperado: " + e.Message;
+            return View("Cadastro", cliente);
+        }
+    }
+    public ActionResult Login()
+    {
+        return View(new Login());
+    }
+
+    [HttpPost]
+    public ActionResult Login(Login model)
+    {
+        User user = repository.LoginUser(model);
+
+        if (user == null)
+        {
+            ViewBag.Error = "Usuário e/ou senha inválidos!";
+            return View(model);
+        }
+
+        HttpContext.Session.SetInt32("idUsuario", user.idUsuario);
+        HttpContext.Session.SetString("nomeUsuario", user.nomeUsuario);
+
+        return RedirectToAction("Index", "Catalogo");
+    }
+
+}
